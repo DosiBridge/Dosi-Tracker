d40856b971dfc0fd3d9acb4cb4bea2ecbdcaeedb
Mihad <mehad65@gmail.com>
2026-07-15 04:28:11 +0600

    Add the Domain.Shared project from the template
    The bottom layer of the ABP solution: constants, the settings definitions,
    and the localization resource. Ships with translations for around 25 languages
    out of the box, though right now the English file only has the four stock keys
    the template puts there (AppName, Menu:Home, Welcome, LongWelcomeMessage).
    
    Nothing tracker-specific in here yet.

---
+++ backend/src/Dosi.Tracker.Domain.Shared/Localization/TrackerResource.cs
@@ -0,0 +1,9 @@
+﻿using Volo.Abp.Localization;
+
+namespace Dosi.Tracker.Localization;
+
+[LocalizationResourceName("Tracker")]
+public class TrackerResource
+{
+
+}
---
+++ backend/src/Dosi.Tracker.Domain.Shared/TrackerDomainErrorCodes.cs
@@ -0,0 +1,6 @@
+﻿namespace Dosi.Tracker;
+
+public static class TrackerDomainErrorCodes
+{
+    /* You can add your business exception error codes here, as constants */
+}
